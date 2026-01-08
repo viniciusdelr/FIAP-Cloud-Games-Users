39a48e03a55f610bef00e223cd487190759e1efc
Gabriel Palheiro <108705306+GabrielPalheiro@users.noreply.github.com>
2026-01-07 21:30:50 -0300

    feat: adicionando rotina para rodar migrations
    2026 01 07

--- FCG/src/FCG.API/Program.cs
+++ FCG/src/FCG.API/Program.cs
@@ -163,4 +163,20 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocorreu um erro ao rodar as migrations.");
+    }
+}
+
 app.Run();
