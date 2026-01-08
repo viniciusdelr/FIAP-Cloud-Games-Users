049aa407247db3cd16ca8acb13fd76a9d43cd597
viniciusdelr <107009079+viniciusdelr@users.noreply.github.com>
2026-01-08 00:11:40 -0300

    feat: library
    2025 01 08

---
+++ FCG/src/FCG.Application/DTOs/LibraryDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FCG.Application.DTOs
+{
+    public class LibraryDto
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; } = string.Empty;
+        public int IdUser { get; set; }
+
+        public int IdGame { get; set; }
+        public DateTime PurchasedDate { get; set; }
+
+        [Precision(18, 2)]
+        public decimal ValuePaid { get; set; }
+    }
+}
---
+++ FCG/src/FCG.Domain/Entities/Library.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FCG.Domain.Entities
+{
+    [Table("Library", Schema = "library")]
+    public class Library
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; } = string.Empty;
+        public int IdUser { get; set; }
+
+        public int IdGame { get; set; }
+        public DateTime PurchasedDate { get; set; }
+
+        [Precision(18,2)]
+        public decimal ValuePaid {  get; set; }
+    }
+}
